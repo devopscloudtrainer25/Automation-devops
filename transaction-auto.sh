9993d39e2218e01ab9772eff77998398f10fbba0
sagargouddevops <sagargouddevops@gmail.com>
2025-03-11 02:12:25 +0000

    ticket-998222 Updating user experience

--- transaction-auto.sh
+++ transaction-auto.sh
@@ -4,3 +4,11 @@ GITHUB - Graphical user Interface
 GIT: Global Interaction tool
 
 Git is an source code management tool where we store all important files, directories, images, logos etc which uses distributed version control system(VCS) mechanism which provides proper tracking and also benefit of storage cost optimization.
+Branching strategy:
+--------------------------------------------------------------------------------------------------------------------------
+Commands: git branch                     // List all local branches
+          git branch -a                  // List both remote and local branches
+          git branch branch_name         // create an branch
+          git checkout branch_name       // switch to branch
+          git checkout -b branch_name    // create and switch branch
+          git branch -D branch_name      // delete an branch
